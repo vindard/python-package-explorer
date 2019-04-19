2470f61ccc6cf491186d1dbc653fe7af5be6b648
User <17693119+vindard@users.noreply.github.com>
2019-04-19 01:11:37 -0400

    convert dict/list mix to strictly dicts

--- indy_explore.py
+++ indy_explore.py
@@ -3,34 +3,34 @@ import indy, json
 class IndyMethods():
 
 	def __init__(self):
-		self.methods_ = [{j: i} for i in dir(indy) for j in dir(getattr(indy, i))]
+		self.methods_ = {j: i for i in dir(indy) for j in dir(getattr(indy, i))}
 
 	def find(self, search):
-		results = []
+		results = {}
 		for m in self.methods_:
-			for v in m.keys():
-				if search in v:
-					results.append(m)
+			if search in m:
+				results[m] = self.methods_[m]
 		#print(json.dumps(results, indent=2))
 		return results
 
-	def __docstring(self, method_dict):
-		k, v = list(method_dict.items())[0]
+	def __docstring(self, k, v):
 		return getattr(getattr(indy, v), k).__doc__
 
 	def __find_w_docstring(self, search):
 		results = self.find(search)
-		descriptions = []
-		for d in results:
-			descriptions.append((d, self.__docstring(d)))
+		descriptions = {}
+		for m in results:
+			descriptions[m] = {
+				'module': results[m],
+				'docstring': self.__docstring(m, results[m])
+				}
 		return descriptions
 
 	def print_docstrings(self, search):
-		docstrings_tuples = self.__find_w_docstring(search)
-		for method in docstrings_tuples:
-			print(method[0])
-			k = list(method[0].keys())[0]
-			print(f"Call with: indy.{method[0][k]}.{k}")
-			print(method[1])
+		b, _b = '\033[1m', '\033[0m'
+		docstrings_dict = self.__find_w_docstring(search)
+		for m in docstrings_dict:
+			print(f"{b}Call with: indy.{docstrings_dict[m]['module']}.{m}{_b}")
+			print(docstrings_dict[m]['docstring'])
 			print('-----\n')
 
